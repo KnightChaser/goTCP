9ddc90acd1085d8f6439ad80c2f0cd220f670d1f
Lee Garam <agerio100@naver.com>
2024-01-12 18:09:06 +0900

    Dockerizing on Windows, tcpserver
    Fuck up the Linux build. I originally built the whole code on Windows and Linux keeps triggering damn stdin error without any precedent guides

---
+++ tcpserver/dockerbuild.ps1
@@ -0,0 +1,4 @@
+docker network remove goTCPnet
+docker network create goTCPnet --subnet=192.168.111.0/24
+docker build . --tag gotcpserver:0.1
+docker run --rm --name gotcpserver --network goTCPnet --ip 192.168.111.111 -p 7777:7777 gotcpserver:0.1
