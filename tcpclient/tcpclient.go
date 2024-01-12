75e2b8e51c7e3f3f985197768fa038ceab29cfae
Lee Garam <agerio100@naver.com>
2024-01-12 11:33:15 +0900

    Channel based async tasking
    Ensure that separating messages from the server connection and messages from the user input(to be sent to the server) via channel, by Go's "select-case" statement.

--- tcpclient/tcpclient.go
+++ tcpclient/tcpclient.go
@@ -5,17 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
 	"strings"
 )
 
 // Write user message on the data stream connected to the server
-func sendMessageToServer(connection net.Conn, message string) {
+func SendMessageToServer(connection net.Conn, message string) {
 	fmt.Fprintf(connection, "%s\n", message)
 }
 
-// Receive and print the server message
-func serverMessageReceiver(connection net.Conn, serverMessageChannel chan string) {
+// Receive and print the message from the server
+func ServerMessageReceiver(connection net.Conn, serverMessageChannel chan string) {
 	serverMessageHandler := bufio.NewScanner(connection)
 	for serverMessageHandler.Scan() {
 		message := serverMessageHandler.Text()
@@ -32,38 +31,42 @@ func main() {
 		fmt.Printf("Error while connecting %s(protocol: %s)\n", accessingAddressPort, protocol)
 		log.Fatal(err)
 		return
+	} else {
+		fmt.Printf("Connected to %s(protocol: %s)!\n", accessingAddressPort, protocol)
+		fmt.Println("Type anything to start!")
 	}
 	defer connection.Close()
 
 	// Read messages from the server, using goroutine
 	serverMessageChannel := make(chan string)
-	go serverMessageReceiver(connection, serverMessageChannel)
-
-	// Print server messages completely first (via channel, synchronously)
-	go func() {
-		for message := range serverMessageChannel {
-			fmt.Printf("Server says: %s\n", message)
-		}
-	}()
+	go ServerMessageReceiver(connection, serverMessageChannel)
 
 	// Read the user input and send messages to the server
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		message := scanner.Text()
+	// select-casing go channels
+	for {
+		select {
+		case message, ok := <-serverMessageChannel:
+			if !ok {
+				// Server messages channel closed, exit
+				fmt.Printf("The connection to the server(%s(protocol: %s)) was closed.\n", accessingAddressPort, protocol)
+				return
+			}
+			fmt.Printf("Server says: %s\n", message)
 
-		// A special triggering keyword to terminate user connection
-		if strings.ToLower(message) == "!exit" {
-			fmt.Printf("Terminating %s connection with the server(%s)\n",
-				strings.ToUpper(protocol), accessingAddressPort)
-			break
+		default:
+			// Read user input and send messages to the server
+			var userInput string
+			fmt.Print("user input> ")
+			fmt.Scanln(&userInput)
+
+			// A special triggering keyword to terminate user connection
+			if strings.ToLower(userInput) == "!exit" {
+				fmt.Printf("Terminating %s connection with the server(%s)\n",
+					strings.ToUpper(protocol), accessingAddressPort)
+				return
+			}
+
+			SendMessageToServer(connection, userInput)
 		}
-
-		sendMessageToServer(connection, message)
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading user input")
-		log.Fatal(err)
-		return
 	}
 }
